ee6ce97d8605fc73f522fdbaadc4c8449ce1be7c
rhyamerta <94899807+rhyamerta@users.noreply.github.com>
2021-11-25 17:05:20 +0700

    Upload Folder Support
    by Hardian 25/11/2021

---
+++ support/formlogin.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../image/icon.JPG">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>RHY Amerta Development</title>
+
+    <style>
+
+        .row{
+            padding-top: 50px;
+        }
+        .kotak1{
+            height: 800px;
+            background-color: #0093E9;
+            background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);
+            margin:auto;
+            
+        }
+
+        .kotak2{
+            height: 500px;
+            background-color: #85FFBD;
+            background-image: linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%);
+            margin:auto;
+            width : 80%;
+            
+        }
+
+        .kotak2 , hr{
+            width:80%;            
+        }
+
+        .textwelcome{
+            text-align:center;
+            font-size: 20px;
+        }
+
+        .textwelcome > span{
+            font-weight:bold;
+        }
+
+        .textwelcome > .please{
+            font-size:17px;
+        }
+
+
+        .kotak3{
+            height:300px;
+            background-color: #FF9A8B;
+            background-image: linear-gradient(90deg, #FF9A8B 0%, #FF6A88 55%, #FF99AC 100%);
+            margin: auto;
+            width:80%;
+            
+        }
+    </style>
+
+  </head>
+  <body>
+
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <div class="kotak1">
+                <h4>kotak1</h4>
+                <div class="kotak2">
+                    <h4>kotak2</h4>
+                        <div class="textwelcome">
+                            Welcome To 
+                            <br>
+                            <span>RHY Amerta Development</span>
+                            <hr>
+                            <div class="please">
+                                please login to continue..
+                            </div>
+                        </div>
+
+                    <div class="kotak3">
+                        <h4>kotak3 </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
